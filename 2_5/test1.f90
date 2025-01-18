1b1f8a5350a95fe98a3ffdba43f1219fd7943693
qplazmfree <qplazmfree@qplazmfrees-MacBook-Pro.local>
2025-01-18 12:01:30 -0500

    updated program

--- 2_5/test1.f90
+++ 2_5/test1.f90
@@ -6,7 +6,7 @@ program test1
     use ModSavePlot,        only:   ModSave_Globe
     use ModAdvance,         only:   ModAdvance_rk4,&
                                     ModAdvance_CommunicateAll
-    use ModParameters,      only:   r_range,&
+    use ModParameters,      only:   MpiSize,MpiRank,r_range,&
                                     ModelS_filename,nSteps,&
                                     nStepsSavePlot,nthSavePlot,nphSavePlot,rSave,CFL
     use ModReadParameters,  only:   ModReadParameters_read
@@ -14,6 +14,7 @@ program test1
                                     YinYangTree_InitTree,&
                                     YinYangTree_SetAll
     use ModStratification,  only:   ModStratification_DoAll
+    use ModInitiation,      only:   ModInitiation_harmonic
     use MPI
 
     implicit none
@@ -21,7 +22,7 @@ program test1
     type(YYTree),target     ::  Tree                        ! The Tree
     integer                 ::  i                           ! The grid
 
-    integer                 ::  ierr,MpiSize,MpiRank        ! For MPI
+    integer                 ::  ierr                        ! For MPI
     integer                 ::  iStep                       ! For step
     character(len=8)        ::  iStep_char                  ! char of istep
     logical                 ::  if_advance
@@ -53,14 +54,14 @@ program test1
             
 
             ! Advance
-            call ModAdvance_rk4(Tree,CFL,.True.,MpiSize,MpiRank,dt)
+            call ModAdvance_rk4(Tree,CFL,.True.,dt)
             if(MpiRank==0)print *,'Complete Advancing at iStep=',iStep,'dt=',dt
 
             ! Write plot
             write(iStep_char,'(I8)')iStep
             if (mod(iStep,nStepsSavePlot)==0) then
                 
-                
+
                 do i=1,len_trim(iStep_char); if (iStep_char(i:i)==' ') iStep_char(i:i)='0'; end do
                 call ModSave_Globe(Tree,rSave,[nthSavePlot,nphSavePlot],filename=iStep_char//'.dat',logical_unit=2)
             end if
@@ -79,47 +80,12 @@ program test1
     contains
 
     subroutine test1_INITIATION
-
         call YinYangTree_InitTree(Tree,r_range)
         call ModAMR_set_grid(Tree)
         call YinYangTree_SetAll(Tree,MpiSize,MpiRank)
         print *,'I"m ',MpiRank,', I have',Tree%nLocalBlocks
-        call ModCommunication_SetGCAll(Tree,MpiSize,MpiRank)
-        call ModAdvance_CommunicateAll(Tree,.false.,MpiSize,MpiRank)
-        call ModAdvance_CommunicateAll(Tree,.false.,MpiSize,MpiRank)
+        call ModCommunication_SetGCAll(Tree)
+        call ModAdvance_CommunicateAll(Tree,.false.)
+        call ModAdvance_CommunicateAll(Tree,.false.)
     end subroutine test1_INITIATION
-
-    subroutine test_harmonic
-        use ModParameters,  only:   ni,nj,nk,ng
-        use ModYinYang,     only:   ModYinyang_CoordConv_0D,&
-                                    ModYinYang_VecConv_0D
-        use ModBlock
-        use ModConst,       only:   dpi
-        implicit none
-        real                    ::  vec(3)
-        real                    ::  coord(3)
-        type(Block),pointer     ::  Block1
-        integer                 ::  iLocalBlock,ir,it,ip
-
-        do iLocalBlock=1,Tree%nLocalBlocks
-            Block1=>Tree%LocalBlocks(iLocalBlock)
-            do ip=-ng+1,ng+nk; do it=-ng+1,ng+nj; do ir=-ng+1,ng+ni
-                Block1%primitive(ir,it,ip,:)=0.000000
-                coord=[Block1%xi(ir),Block1%xj(it),Block1%xk(ip)]
-                if (.not. Block1%if_yin) coord=ModYinyang_CoordConv_0D(coord)
-                vec=sin(dpi*(coord(1)-r_range(1))/(r_range(2)-r_range(1)))*[1.,1.,1.]*1.e-2*&
-                    sin(dpi*4.*coord(3))*sin(coord(2))**4
-                Block1%primitive(ir,it,ip,2:4)=vec
-                if (.not. Block1%if_yin) then
-                    Block1%primitive(ir,it,ip,2:4)=&
-                    ModYinYang_VecConv_0D(coord,vec)
-                end if
-            end do; end do; end do
-            Block1%primitive_rk=Block1%primitive
-        end do
-        call ModSave_Globe(Tree,rSave,[nthSavePlot,nphSavePlot],filename='-00000000.dat',logical_unit=2)
-        call ModAdvance_CommunicateAll(Tree,.false.,MpiSize,MpiRank)
-        call ModSave_Globe(Tree,rSave,[nthSavePlot,nphSavePlot],filename='+00000000.dat',logical_unit=2)
-    end subroutine test_harmonic
-
 end program test1
